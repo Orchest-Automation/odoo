3c58134315ac732f0869e687b73a6b46503cb411
Christopher Ormaza <chris.ormaza@gmail.com>
2026-03-23 20:23:00 -0500

    imp: removed update service

--- addons/mail/models/update.py
+++ addons/mail/models/update.py
@@ -27,13 +27,13 @@ class PublisherWarrantyContract(AbstractModel):
         dbuuid = IrParamSudo.get_param('database.uuid')
         db_create_date = IrParamSudo.get_param('database.create_date')
         limit_date = fields.Datetime.now() - datetime.timedelta(15)
-        nbr_users = Users.search_count([('active', '=', True)])
-        nbr_active_users = Users.search_count([("login_date", ">=", limit_date), ('active', '=', True)])
+        nbr_users = Users.search_count([('active', '=', True)], limit=5)
+        nbr_active_users = Users.search_count([("login_date", ">=", limit_date), ('active', '=', True)], limit=5)
         nbr_share_users = 0
         nbr_active_share_users = 0
         if "share" in Users._fields:
-            nbr_share_users = Users.search_count([("share", "=", True), ('active', '=', True)])
-            nbr_active_share_users = Users.search_count([("share", "=", True), ("login_date", ">=", limit_date), ('active', '=', True)])
+            nbr_share_users = Users.search_count([("share", "=", True), ('active', '=', True)], limit=10)
+            nbr_active_share_users = Users.search_count([("share", "=", True), ("login_date", ">=", limit_date), ('active', '=', True)], limit=15)
         user = self.env.user
         domain = [('application', '=', True), ('state', 'in', ['installed', 'to upgrade', 'to remove'])]
         apps = self.env['ir.module.module'].sudo().search_read(domain, ['name'])
@@ -75,42 +75,42 @@ class PublisherWarrantyContract(AbstractModel):
         return literal_eval(r.text)
 
     def update_notification(self, cron_mode=True):
-        """
-        Send a message to Odoo's publisher warranty server to check the
-        validity of the contracts, get notifications, etc...
+        # """
+        # Send a message to Odoo's publisher warranty server to check the
+        # validity of the contracts, get notifications, etc...
 
-        @param cron_mode: If true, catch all exceptions (appropriate for usage in a cron).
-        @type cron_mode: boolean
-        """
-        try:
-            try:
-                result = self._get_sys_logs()
-            except Exception:
-                if cron_mode:   # we don't want to see any stack trace in cron
-                    return False
-                _logger.debug("Exception while sending a get logs messages", exc_info=1)
-                raise UserError(_("Error during communication with the publisher warranty server."))
-            # old behavior based on res.log; now on mail.message, that is not necessarily installed
-            user = self.env['res.users'].sudo().browse(SUPERUSER_ID)
-            poster = self.sudo().env.ref('mail.channel_all_employees')
-            for message in result["messages"]:
-                try:
-                    poster.message_post(body=message, subtype_xmlid='mail.mt_comment', partner_ids=[user.partner_id.id])
-                except Exception:
-                    pass
-            if result.get('enterprise_info'):
-                # Update expiration date
-                set_param = self.env['ir.config_parameter'].sudo().set_param
-                set_param('database.expiration_date', result['enterprise_info'].get('expiration_date'))
-                set_param('database.expiration_reason', result['enterprise_info'].get('expiration_reason', 'trial'))
-                set_param('database.enterprise_code', result['enterprise_info'].get('enterprise_code'))
-                set_param('database.already_linked_subscription_url', result['enterprise_info'].get('database_already_linked_subscription_url'))
-                set_param('database.already_linked_email', result['enterprise_info'].get('database_already_linked_email'))
-                set_param('database.already_linked_send_mail_url', result['enterprise_info'].get('database_already_linked_send_mail_url'))
+        # @param cron_mode: If true, catch all exceptions (appropriate for usage in a cron).
+        # @type cron_mode: boolean
+        # """
+        # try:
+        #     try:
+        #         result = self._get_sys_logs()
+        #     except Exception:
+        #         if cron_mode:   # we don't want to see any stack trace in cron
+        #             return False
+        #         _logger.debug("Exception while sending a get logs messages", exc_info=1)
+        #         raise UserError(_("Error during communication with the publisher warranty server."))
+        #     # old behavior based on res.log; now on mail.message, that is not necessarily installed
+        #     user = self.env['res.users'].sudo().browse(SUPERUSER_ID)
+        #     poster = self.sudo().env.ref('mail.channel_all_employees')
+        #     for message in result["messages"]:
+        #         try:
+        #             poster.message_post(body=message, subtype_xmlid='mail.mt_comment', partner_ids=[user.partner_id.id])
+        #         except Exception:
+        #             pass
+        #     if result.get('enterprise_info'):
+        #         # Update expiration date
+        #         set_param = self.env['ir.config_parameter'].sudo().set_param
+        #         set_param('database.expiration_date', result['enterprise_info'].get('expiration_date'))
+        #         set_param('database.expiration_reason', result['enterprise_info'].get('expiration_reason', 'trial'))
+        #         set_param('database.enterprise_code', result['enterprise_info'].get('enterprise_code'))
+        #         set_param('database.already_linked_subscription_url', result['enterprise_info'].get('database_already_linked_subscription_url'))
+        #         set_param('database.already_linked_email', result['enterprise_info'].get('database_already_linked_email'))
+        #         set_param('database.already_linked_send_mail_url', result['enterprise_info'].get('database_already_linked_send_mail_url'))
 
-        except Exception:
-            if cron_mode:
-                return False    # we don't want to see any stack trace in cron
-            else:
-                raise
+        # except Exception:
+        #     if cron_mode:
+        #         return False    # we don't want to see any stack trace in cron
+        #     else:
+        #         raise
         return True
